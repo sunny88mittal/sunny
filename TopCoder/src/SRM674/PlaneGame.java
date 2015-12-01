48112a366bfec5dc02fce78019f865e081a550e0
Sunny Mittal <sunny.mittal@lybrate.com>
2015-12-02 00:29:42 +0530

    Adding some more code

--- TopCoder/src/SRM674/PlaneGame.java
+++ TopCoder/src/SRM674/PlaneGame.java
@@ -34,16 +34,29 @@ public class PlaneGame {
 			}
 		}
 
-		//TODO :: Finish this code
+		//TODO : fix this
 		// Rotate the points
 		for (int i = 0; i < n; i++) {
-			int count = 0;
-			if (x[i] != 0 || y[i] != 0) {
+			if (x[i] != 0 && y[i] != 0) {
+				int count = 1;
+				double angle = Math.atan(Math.abs(y[i]) / Math.abs(x[i]));
 				for (int j = 0; j < n; j++) {
-					if (count > result) {
-						result = count;
+					if (i != j) {
+						double tempyi = Math.abs(y[j]) - Math.abs(y[j]) * Math.sin(angle);
+						if (tempyi == 0 || x[j] == 0) {
+							count++;
+							continue;
+						}
+						double tempxi = Math.abs(x[j]) - Math.abs(x[j]) * Math.cos(angle);
+						if (tempxi == 0 || y[j] == 0) {
+							count++;
+							continue;
+						}
 					}
-				}	
+				}
+				if (count > result) {
+					result = count;
+				}
 			}
 		}
 
@@ -52,6 +65,7 @@ public class PlaneGame {
 
 	public static void main(String args[]) {
 		PlaneGame g = new PlaneGame();
-		System.out.println(g.bestShot(new int[] { 0, 5 }, new int[] { 0, 5 }));
+		System.out.println(g.bestShot(new int[] { 0, -1, 1, 1, -1 }, new int[] {
+				0, -1, -1, 1, 1 }));
 	}
 }
