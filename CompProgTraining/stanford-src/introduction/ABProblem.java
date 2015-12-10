c9665e97ebdfe85d93a005ddeb1875149adcabdb
Sunny Mittal <sunny.mittal@lybrate.com>
2015-12-11 01:08:46 +0530

    Stanford Course Competitive Training

---
+++ CompProgTraining/stanford-src/introduction/ABProblem.java
@@ -0,0 +1,11 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class ABProblem {
+	public static void main(String args[]) throws Exception {
+		Scanner cin = new Scanner(System.in);
+		int a = cin.nextInt(), b = cin.nextInt();
+		System.out.println(a + b);
+	}
+}
