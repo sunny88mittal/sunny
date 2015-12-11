c4227e5f54d67c904c58374063e87d6efc167146
Sunny Mittal <sunny.mittal@lybrate.com>
2015-12-11 23:56:55 +0530

    Fixed Exponential

--- CompProgTraining/stanford-src/introduction/Exponentiation.java
+++ CompProgTraining/stanford-src/introduction/Exponentiation.java
@@ -4,28 +4,19 @@ import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Exponentiation {
-
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) {
 		Scanner cin = new Scanner(System.in);
-		String str = cin.nextLine();
-		while (str != null) {
-			str = str.replaceAll("  ", " ");
-			String tokens[] = str.split(" ");
-			int exp = Integer.parseInt(tokens[1]);
-			System.out.println(getExp(new BigDecimal(tokens[0]), exp).toPlainString());
-			str = cin.nextLine();
-		}
-	}
-
-	private static BigDecimal getExp(BigDecimal bg, int exp) {
-		if (exp == 1) {
-			return bg;
-		}
-
-		if (exp % 2 == 0) {
-			return getExp(bg, exp / 2).multiply(getExp(bg, exp / 2));
-		} else {
-			return getExp(bg, exp / 2).multiply(getExp(bg, exp / 2)).multiply(bg);
+		while (cin.hasNext()) {
+			BigDecimal b = cin.nextBigDecimal();
+			int n = cin.nextInt();
+			BigDecimal a = BigDecimal.valueOf(1);
+			for (int i = 1; i <= n; i++)
+				a = a.multiply(b);
+			a = a.stripTrailingZeros();
+			String pre = a.toPlainString();
+			if (pre.startsWith("0."))
+				pre = pre.substring(1);
+			System.out.println(pre);
 		}
 	}
 }
