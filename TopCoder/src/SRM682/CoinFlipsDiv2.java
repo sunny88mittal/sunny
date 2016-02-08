921ba15d641866074d34b10c7bfaa46901419718
SunnyMittal <sunny88mittal@gmail.com>
2016-02-08 19:11:42 +0530

    SRM 682 Div2 250

---
+++ TopCoder/src/SRM682/CoinFlipsDiv2.java
@@ -0,0 +1,25 @@
+package SRM682;
+
+public class CoinFlipsDiv2 {
+
+	public int countCoins(String state) {
+		int length = state.length();
+		int count = 0;
+		for (int i=0; i<length; i++) {
+			if (i > 0) {
+				if (state.charAt(i-1) != state.charAt(i)) {
+					count++;
+					continue;
+				}	
+			}
+			
+			if (i < length-1) {
+				if (state.charAt(i) != state.charAt(i+1)) {
+					count++;
+					continue;
+				}
+			}
+		}
+		return count;
+	}
+}
