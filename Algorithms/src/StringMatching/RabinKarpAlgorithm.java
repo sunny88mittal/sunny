1d1b07f9e55fa745b5e6b1bf16840fea23160407
SunnyMittal <sunny88mittal@gmail.com>
2016-02-12 20:03:57 +0530

    Rabin Karp Algo code -- Still failing at calculating the shift code correctly

--- Algorithms/src/StringMatching/RabinKarpAlgorithm.java
+++ Algorithms/src/StringMatching/RabinKarpAlgorithm.java
@@ -1,9 +1,63 @@
 package StringMatching;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 public class RabinKarpAlgorithm {
-	
-	public static void main (String args[]) {
-		
+
+	private static int d = 256;
+
+	private static int q = BigInteger.probablePrime(12, new Random()).intValue();
+
+	public static void main(String args[]) {
+		String pattern = "ABABAC";
+		String text = "AABABACAABCABABAC";
+		findMatches(pattern, text);
 	}
 
+	/**
+	 * Finds the pattern using the Rabin Karp Algorithm
+	 * 
+	 * @param pattern
+	 * @param text
+	 */
+	private static void findMatches(String pattern, String text) {
+		char[] pc = pattern.toCharArray();
+		char[] tc = text.toCharArray();
+
+		int pMod = 0;
+		int tMod = 0;
+		int pl = pattern.length();
+		int exp = findExp(pl - 1);
+
+		// Calculate initial values
+		for (int i = 0; i < pl; i++) {
+			pMod = (pMod + pc[i] * d) % q;
+			tMod = (tMod + tc[i] * d) % q;
+		}
+
+		// Check the pattern in the text
+		for (int s = 0; s <= text.length() - pl; s++) {
+			// Validate if the match is correct
+			if (pMod == tMod) {
+				if (pattern.equals(text.substring(s, pl))) {
+					System.out.println("Match found at :" + s);
+				}
+			}
+
+			// Shift to next characters in text
+			if (s < text.length() - pl) {
+				tMod = (tMod + q - exp * tc[s] % q) % q;
+				tMod = (tMod * d + tc[s + pl]) % q;
+			}
+		}
+	}
+
+	private static int findExp(int pow) {
+		int result = 1;
+		for (int i = 0; i < pow; i++) {
+			result = (result * d) % q;
+		}
+		return result;
+	}
 }
