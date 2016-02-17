7ff684bb6d59933634bf51146798e78edf415444
SunnyMittal <sunny88mittal@gmail.com>
2016-02-17 14:07:33 +0530

    Simpler Solution

--- TopCoder/src/SRM585/TrafficCongestionDivTwo.java
+++ TopCoder/src/SRM585/TrafficCongestionDivTwo.java
@@ -3,30 +3,18 @@ package SRM585;
 public class TrafficCongestionDivTwo {
 
 	public long theMinCars(int treeHeight) {
-		long minCars = 0;
-		int arraysize = treeHeight + 1;
-		if (treeHeight < 2) {
-			minCars = 1;
-		} else {
-			long[] minCarsArray = new long[arraysize];
+		long[] minCarsArray = new long[treeHeight + 1];
 
-			minCarsArray[0] = 1;
-			minCarsArray[1] = 1;
+		minCarsArray[0] = 1;
+		minCarsArray[1] = 1;
 
-			for (int i = 2; i < arraysize; i++) {
-				int toStop = i - 2;
-				long sum = 0;
-				for (int j = 0; j <= toStop; j++) {
-					sum += minCarsArray[j];
-				}
-				minCarsArray[i] = 2 * sum + 1;
+		for (int i = 2; i <= treeHeight; i++) {
+			minCarsArray[i] = 1;
+			for (int j = 0; j <= i - 2; j++) {
+				minCarsArray[i] += 2 * minCarsArray[j];
 			}
-
-			minCars = minCarsArray[treeHeight];
 		}
 
-		return minCars;
+		return minCarsArray[treeHeight];
 	}
 }
-
-//Check if better solution possible
