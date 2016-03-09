b50fb6fb5cd6f456b92a93b572504f6742e23a3b
SunnyMittal <sunny88mittal@gmail.com>
2016-03-09 13:29:53 +0530

    Adding few files

---
+++ GoogleCodeJam/src/Template.java
@@ -0,0 +1,29 @@
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class Template {
+
+	private static String smallFile = "/home/priyanka/workspace/GoogleCodeJam/src/two/zero/eight/onea/A-small-practice.in";
+
+	private static String largeFile = "/home/priyanka/workspace/GoogleCodeJam/src/two/zero/eight/onea/A-large-practice.in";
+
+	public static void main(String args[]) throws Exception {
+		processFile(smallFile);
+		processFile(largeFile);
+	}
+
+	private static void processFile(String input) throws Exception {
+		BufferedReader br = new BufferedReader(new FileReader(input));
+		PrintWriter pw = new PrintWriter(new FileWriter(input.replace("practice", "solution")));
+		int testCases = Integer.parseInt(br.readLine());
+		
+
+		br.close();
+		pw.flush();
+		pw.close();
+	}
+}
