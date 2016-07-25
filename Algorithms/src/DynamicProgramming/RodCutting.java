fc66ccd41e1bbb57d0f4579a27da7b0b5d9e5f15
SunnyMittal <sunny88mittal@gmail.com>
2016-07-25 19:20:25 +0530

    abc

---
+++ Algorithms/src/DynamicProgramming/RodCutting.java
@@ -0,0 +1,37 @@
+package DynamicProgramming;
+
+class RodCutting {
+	// A utility function to get the maximum of two integers
+	static int max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+
+	/*
+	 * Returns the best obtainable price for a rod of length n and price[] as
+	 * prices of different pieces
+	 */
+	static int cutRod(int price[], int n) {
+		int val[] = new int[n + 1];
+		val[0] = 0;
+		int i, j;
+
+		// Build the table val[] in bottom up manner and return the last entry
+		// from the table
+		for (i = 1; i <= n; i++) {
+			int max_val = Integer.MIN_VALUE;
+			for (j = 0; j < i; j++)
+				max_val = max(max_val, price[j] + val[i - j - 1]);
+			val[i] = max_val;
+		}
+
+		return val[n];
+	}
+
+	/* Driver program to test above functions */
+	public static void main(String args[]) {
+		int arr[] = new int[] { 1, 5, 8, 9, 10, 17, 17, 20 };
+		int size = arr.length;
+		System.out.println("Maximum Obtainable Value is " + cutRod(arr, size));
+
+	}
+}
