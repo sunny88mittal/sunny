568ae85652651727ce3d854c28168977b2af2011
SunnyMittal <sunny88mittal@gmail.com>
2016-07-28 21:34:54 +0530

    Transforming collections

---
+++ JavaPracticeNew/lambdaexpressions/chapter2/TransformingACollection.java
@@ -0,0 +1,28 @@
+package chapter2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TransformingACollection {
+
+	public static void main(String args[]) {
+		final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+		
+		//Using for each and a new list to store output
+		final List<String> uppercaseNames = new ArrayList<String>();
+		friends.forEach(name -> uppercaseNames.add(name.toUpperCase()));
+		System.out.println(uppercaseNames);
+		
+		//Using Streams
+		friends.stream()
+		       .map(name -> name.toUpperCase())
+		       .forEach(name -> System.out.print(name + " "));
+		System.out.println();
+		
+		//Using method reference
+		friends.stream()
+		       .map(String::toUpperCase) //changed in this line
+		       .forEach(name -> System.out.print(name + " "));
+	}
+}
