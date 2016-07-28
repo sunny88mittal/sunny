8a32469e39a23795911eb677308242e70d5dd644
SunnyMittal <sunny88mittal@gmail.com>
2016-07-28 21:02:04 +0530

    abc

---
+++ JavaPracticeNew/lambdaexpressions/chapter1/DiscountedPricesSum.java
@@ -0,0 +1,22 @@
+package chapter1;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+public class DiscountedPricesSum {
+
+	public static void main(String args[]) {
+		List<BigDecimal> prices = Arrays.asList(new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
+				new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"), new BigDecimal("45"),
+				new BigDecimal("12"));
+		// total the prices greater than $20, discounted by 10%.
+
+		BigDecimal totalOfDiscountedPrices = prices.stream()
+		        .filter(price -> price.compareTo(BigDecimal.valueOf(20)) > 0)
+				.map(price -> price.multiply(BigDecimal.valueOf(0.9)))
+				.reduce(BigDecimal.ZERO, BigDecimal::add);
+		
+		System.out.println(totalOfDiscountedPrices);
+	}
+}
