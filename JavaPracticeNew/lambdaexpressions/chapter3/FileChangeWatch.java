9d29a4b14c17991eccd70dec0adadbf6b32221ab
SunnyMittal <sunny88mittal@gmail.com>
2016-08-01 12:08:33 +0530

    File change watch class. Interesting facility provided by Java. Could be used to build a Zookeper like system

---
+++ JavaPracticeNew/lambdaexpressions/chapter3/FileChangeWatch.java
@@ -0,0 +1,26 @@
+package chapter3;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.concurrent.TimeUnit;
+
+public class FileChangeWatch {
+
+	public static void main(String args[]) throws IOException, InterruptedException {
+		String path1 = "/home/priyanka/Documents/computers/Dbms";
+
+		final Path path = Paths.get(path1);
+		final WatchService watchService = path.getFileSystem().newWatchService();
+		path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+		System.out.println("Report any file changed within next 1 minutes...");
+
+		final WatchKey watchKey = watchService.poll(1, TimeUnit.MINUTES);
+		if (watchKey != null) {
+			watchKey.pollEvents().stream().forEach(event -> System.out.println(event.context()));
+		}
+	}
+}
