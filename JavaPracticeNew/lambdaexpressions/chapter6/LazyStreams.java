3e724e06414310738b6fca5268e49cc0de535fe2
SunnyMittal <sunny88mittal@gmail.com>
2016-08-03 02:51:09 +0530

    LAzy evaluation in streams. Nothing new, if we write code oursevlves we will also do minimal work

---
+++ JavaPracticeNew/lambdaexpressions/chapter6/LazyStreams.java
@@ -0,0 +1,28 @@
+package chapter6;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LazyStreams {
+
+	private static int length(final String name) {
+		System.out.println("getting length for " + name);
+		return name.length();
+	}
+
+	private static String toUpper(final String name) {
+		System.out.println("converting to uppercase: " + name);
+		return name.toUpperCase();
+	}
+
+	public static void main(final String[] args) {
+		List<String> names = Arrays.asList("Brad", "Kate", "Kim", "Jack", "Joe", "Mike", "Susan", "George", "Robert",
+				"Julia", "Parker", "Benson");
+		final String firstNameWith3Letters = names
+				.stream()
+				.filter(name -> length(name) == 3)
+				.map(name -> toUpper(name))
+				.findFirst().get();
+		System.out.println(firstNameWith3Letters);
+	}
+}
