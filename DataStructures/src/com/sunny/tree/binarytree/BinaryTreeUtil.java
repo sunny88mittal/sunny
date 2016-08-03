7d04e5bb5eae7fc3abb41ab86bf7e459a07326a9
SunnyMittal <sunny88mittal@gmail.com>
2016-08-03 22:13:30 +0530

    small change

--- DataStructures/src/com/sunny/tree/binarytree/BinaryTreeUtil.java
+++ DataStructures/src/com/sunny/tree/binarytree/BinaryTreeUtil.java
@@ -9,7 +9,7 @@ import com.sunny.tree.binaryseacrhtree.TreeNode;
 public class BinaryTreeUtil {
 
 	/**
-	 * Get binary tree by giving the input as in level order as works only for
+	 * Get binary tree by giving the input as in level order as works only for full tree
 	 * Separate levels by -
 	 * Separate nodes at same level by ,
 	 * complete trees 1-2,3-4,5,6,7-8,9,10,11,12,13,14,15 
