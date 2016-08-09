511975d999e3aa06e18c78e2c63eea576467ad5b
SunnyMittal <sunny88mittal@gmail.com>
2016-08-09 16:33:58 +0530

    Adding back

---
+++ Hadoop/learning/chapter21/MaxTemperature.java
@@ -0,0 +1,37 @@
+
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class MaxTemperature {
+
+	public static void main(String[] args) throws Exception {
+		if (args.length != 2) {
+			System.err.println("Usage: MaxTemperature <input path> <output path>");
+			System.exit(-1);
+		}
+		
+		//Creates a job
+		Job job = new Job();
+		job.setJarByClass(MaxTemperature.class);
+		job.setJobName("Max temperature");
+		
+		//Specifies paths from where to read input and produce output
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		//Set the map and reduce classes
+		job.setMapperClass(MaxTemperatureMapper.class);
+		job.setReducerClass(MaxTemperatureReducer.class);
+		
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
