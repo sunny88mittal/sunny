a8ab129cfa69354c1ed726c37b1dd17eecef0cc6
sunmitta <sunmitta@sunmitta.fareast.corp.microsoft.com>
2019-03-31 12:52:06 +0530

    Added Moving Average Logic

--- StocksAnalyst/src/com/sunny/indicators/MovingAverage.java
+++ StocksAnalyst/src/com/sunny/indicators/MovingAverage.java
@@ -2,4 +2,32 @@ package com.sunny.indicators;
 
 public class MovingAverage {
 
+	private MovingAverageType type;
+
+	public static enum MovingAverageType {
+		SIMPLE, EXPONENTIAL
+	}
+
+	public MovingAverage(MovingAverageType type) {
+		this.type = type;
+	}
+
+	public float getValue(Float[] data) {
+		float movingAvergae = 0;
+		switch (type) {
+		case SIMPLE:
+			int length = data.length;
+			float sum = 0;
+			for (float point : data) {
+				sum += point;
+			}
+			movingAvergae = sum / length;
+			break;
+		case EXPONENTIAL:
+			break;
+		default:
+			break;
+		}
+		return movingAvergae;
+	}
 }
