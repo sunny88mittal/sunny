b6651074a5121aa7633f7a61a5c8ceae54dc0877
sunmitta <sunmitta@MAQ41003.redmond.corp.microsoft.com>
2019-04-07 14:48:49 +0530

    Curator

---
+++ Curator/src/main/java/curator/Constants.java
@@ -0,0 +1,10 @@
+package curator;
+
+public class Constants {
+
+	public static final String CONNECTION_STRING = "localhost:2181";
+
+	public static final String MEMEBER_NODE = "/member";
+	
+	public static final String MASTER_NODE = "/master";
+}
---
+++ Curator/src/main/java/curator/masterelection/ParticipatingNode.java
@@ -0,0 +1,84 @@
+package curator.masterelection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.leader.LeaderLatch;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import curator.Constants;
+
+public class ParticipatingNode implements Runnable {
+
+	private final String nodeName;
+
+	private final String connectionStirng;
+
+	private final String masterNodePath;
+
+	public ParticipatingNode(String nodeName, String connectionStirng, String masterNodePath) {
+		super();
+		this.nodeName = nodeName;
+		this.connectionStirng = connectionStirng;
+		this.masterNodePath = masterNodePath;
+	}
+
+	public void run() {
+		// Create client
+		RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+		CuratorFramework client = CuratorFrameworkFactory.newClient(connectionStirng, retryPolicy);
+		client.start();
+
+		// Create latch
+		LeaderLatch leaderLatch = new LeaderLatch(client, masterNodePath, nodeName);
+
+		// Participate in election
+		try {
+			leaderLatch.start();
+			leaderLatch.await();
+			if (leaderLatch.hasLeadership()) {
+				System.out.println("Current leader is:" + nodeName);
+			}
+			Thread.sleep(10000);
+			leaderLatch.close();
+		} catch (Exception e) {
+			System.out.println("Exception in " + nodeName + e.getMessage());
+		} finally {
+			// Close client
+			client.close();
+		}
+	}
+
+	public static void main(String args[]) throws InterruptedException {
+		int count = 5;
+		List<Thread> participatingNodesThreads = getParticipatingNodesThreads(count);
+
+		// Start threads
+		System.out.println("Starting threads");
+		for (Thread th : participatingNodesThreads) {
+			th.start();
+		}
+
+		// Wait for them to complete
+		System.out.println("Waiting for threads");
+		for (Thread th : participatingNodesThreads) {
+			th.join();
+		}
+
+		System.out.println("Finished");
+	}
+
+	private static List<Thread> getParticipatingNodesThreads(int count) {
+		List<Thread> participatingNodesThreads = new ArrayList<Thread>();
+		for (int i = 0; i < count; i++) {
+			ParticipatingNode node = new ParticipatingNode("ParticipatingNode-" + i, Constants.CONNECTION_STRING,
+					Constants.MASTER_NODE);
+			Thread th = new Thread(node);
+			participatingNodesThreads.add(th);
+		}
+		return participatingNodesThreads;
+	}
+}
