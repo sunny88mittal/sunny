96866ddc2473b4db5dc2aa562d009cb953c775a9
sunny88mittal <sunny88mittal@gmail.com>
2019-07-30 15:43:24 +0530

    Adding support to read options chain

--- Analyst/src/main/java/Constants/FileConstants.java
+++ Analyst/src/main/java/Constants/FileConstants.java
@@ -7,4 +7,6 @@ public class FileConstants {
 	public static String DATA_FILE_BASE_PATH = FILE_BASE_PATH + "Data\\";
 
 	public static String ANALYSIS_FILE_BASE_PATH = FILE_BASE_PATH + "Analysis\\";
+	
+	public static String OPTIONS_FILE_BASE_PATH = FILE_BASE_PATH + "Options\\";
 }
