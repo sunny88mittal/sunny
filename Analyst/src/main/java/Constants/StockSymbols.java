8abf530f0f4b9481bca854f198afcdf62aa32816
sunny88mittal <sunny88mittal@gmail.com>
2019-08-02 17:44:38 +0530

    Some changes

--- Analyst/src/main/java/Constants/StockSymbols.java
+++ Analyst/src/main/java/Constants/StockSymbols.java
@@ -1,35 +1,65 @@
 package Constants;
 
-public class StockSymbols {
+import java.util.ArrayList;
+import java.util.List;
 
-	public static String NIFTY = "NIFTY";
+public enum StockSymbols {
 
-	public static String BAJAJ_FINANCE = "BAJFINANCE";
-
-	public static String SRIRAM_TRANSPORTATION_FINANCE = "SRTRANSFIN";
-
-	public static String MARUTI_SUZUKI = "MARUTI";
-
-	public static String RELIANCE = "RELIANCE";
-
-	public static String HDFC = "HDFC";
-
-	public static String HDFC_BANK = "HDFCBANK";
-
-	public static String KOTAK_BANK = "KOTAKBANK";
-
-	public static String INDUSIND_BANK = "INDUSINDBK";
-
-	public static String AXIS_BANK = "AXISBANK";
-
-	public static String ICICI_BANK = "ICICIBANK";
-
-	public static String YES_BANK = "YESBANK";
-
-	public static String RBL_BANK = "RBLBANK";
+	NIFTY("NIFTY", "256265"), 
 	
-	public static String SBI_BANK = "SBIN";
+	BANK_NIFTY("BANKNIFTY", "260105"),
+	
+	BAJAJ_FINANCE("BAJFINANCE", "81153"), 
+	
+	SRIRAM_TRANSPORTATION_FINANCE("SRTRANSFIN", "1102337"),
 
-	public static String BANK_NIFTY = "BANKNIFTY";
+	MARUTI_SUZUKI("MARUTI", ""),
 
+	RELIANCE("RELIANCE", "738561"),
+
+	HDFC("HDFC", "340481"),
+
+	HDFC_BANK("HDFCBANK", "341249"),
+
+	KOTAK_BANK("KOTAKBANK", "492033"),
+
+	INDUSIND_BANK("INDUSINDBK", "1346049"),
+
+	AXIS_BANK("AXISBANK", "1510401"),
+
+	ICICI_BANK("ICICIBANK", "1270529"),
+
+	YES_BANK("YESBANK", "3050241"),
+
+	RBL_BANK("RBLBANK", "4708097"),
+
+	SBI_BANK("SBIN", "779521");
+
+	public String name;
+
+	public String code;
+
+	private StockSymbols(String name, String code) {
+		this.name = name;
+		this.code = code;
+	}
+	
+	public static List<StockSymbols> getAllStocksList() {
+		List<StockSymbols> stockSymbolsList = new ArrayList<StockSymbols>();
+		stockSymbolsList.add(NIFTY);
+		stockSymbolsList.add(BANK_NIFTY);
+		stockSymbolsList.add(BAJAJ_FINANCE);
+		stockSymbolsList.add(SRIRAM_TRANSPORTATION_FINANCE);
+		stockSymbolsList.add(MARUTI_SUZUKI);
+		stockSymbolsList.add(RELIANCE);
+		stockSymbolsList.add(HDFC);
+		stockSymbolsList.add(HDFC_BANK);
+		stockSymbolsList.add(INDUSIND_BANK);
+		stockSymbolsList.add(AXIS_BANK);
+		stockSymbolsList.add(ICICI_BANK);
+		stockSymbolsList.add(YES_BANK);
+		stockSymbolsList.add(RBL_BANK);
+		stockSymbolsList.add(SBI_BANK);
+		return stockSymbolsList;
+	}
 }
--- Analyst/src/main/java/Data/StocksDataDownloader.java
+++ Analyst/src/main/java/Data/StocksDataDownloader.java
@@ -9,8 +9,8 @@ import Constants.StockSymbols;
 public class StocksDataDownloader {
 
 	private static final String URL = "https://kitecharts-aws.zerodha.com" + "/api/chart/SYMBOL/INTERVAL?"
-			+ "public_token=px7RlNQX3W7rg9Vm2WOUDbGzo6WBxUqy&" + "user_id=YF3220&oi=1" + "&api_key=kitefront&"
-			+ "access_token=&" + "from=2014-01-01" + "&to=2019-07-28&" + "ciqrandom=1564301727399";
+			+ "public_token=px7RlNQX3W7rg9Vm2WOUDbGzo6WBxUqy&" + "user_id=YF3210&oi=1" + "&api_key=kitefront&"
+			+ "access_token=&" + "from=2014-01-01" + "&to=2019-08-02&" + "ciqrandom=1564301727399";
 
 	public static void getData(String stockName, String stockSymbol, String interval) throws IOException {
 		IOHelper.createDirIfReq(FileConstants.DATA_FILE_BASE_PATH, stockName);
@@ -19,18 +19,14 @@ public class StocksDataDownloader {
 		String data = NetworkHelper.makeGetRequest(url);
 		IOHelper.writeToFile(fileLocation, data);
 	}
-	
+
 	private static void getDataForStock(String stockName, String stockSymbol) throws IOException {
-		getData(stockName, stockSymbol, CandleStickInterval.MINUTE_3);
-		getData(stockName, stockSymbol, CandleStickInterval.MINUTE_5);
-		getData(stockName, stockSymbol, CandleStickInterval.MINUTE_10);
-		getData(stockName, stockSymbol, CandleStickInterval.MINUTE_15);
-		getData(stockName, stockSymbol, CandleStickInterval.MINUTE_30);
-		getData(stockName, stockSymbol, CandleStickInterval.MINUTE_60);
-		getData(stockName, stockSymbol, CandleStickInterval.DAY);
+		for (String interval : CandleStickInterval.getAllIntervals()) {
+			getData(stockName, stockSymbol, interval);
+		}
 	}
 
 	public static void main(String args[]) throws IOException {
-		getDataForStock(StockSymbols.SBI_BANK, "779521");
+		getDataForStock(StockSymbols.BANK_NIFTY.name, "260105");
 	}
 }
