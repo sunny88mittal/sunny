66088f630aefa1577303349038760df609bd4209
sunny88mittal <sunny88mittal@gmail.com>
2019-08-02 17:49:52 +0530

    Minor change

--- Analyst/src/main/java/Data/StocksDataDownloader.java
+++ Analyst/src/main/java/Data/StocksDataDownloader.java
@@ -10,7 +10,7 @@ public class StocksDataDownloader {
 
 	private static final String URL = "https://kitecharts-aws.zerodha.com" + "/api/chart/SYMBOL/INTERVAL?"
 			+ "public_token=px7RlNQX3W7rg9Vm2WOUDbGzo6WBxUqy&" + "user_id=YF3210&oi=1" + "&api_key=kitefront&"
-			+ "access_token=&" + "from=2014-01-01" + "&to=2019-08-02&" + "ciqrandom=1564301727399";
+			+ "access_token=&" + "from=2014-01-01" + "&to=2019-08-01&" + "ciqrandom=1564301727399";
 
 	public static void getData(String stockName, String stockSymbol, String interval) throws IOException {
 		IOHelper.createDirIfReq(FileConstants.DATA_FILE_BASE_PATH, stockName);
@@ -27,6 +27,6 @@ public class StocksDataDownloader {
 	}
 
 	public static void main(String args[]) throws IOException {
-		getDataForStock(StockSymbols.BANK_NIFTY.name, "260105");
+		getDataForStock(StockSymbols.BANK_NIFTY.name, StockSymbols.BANK_NIFTY.code);
 	}
 }
