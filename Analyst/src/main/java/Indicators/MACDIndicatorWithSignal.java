343efc03c07a0cd3244b0b57c5c52a2e59261386
sunny88mittal <sunny88mittal@gmail.com>
2019-08-03 19:29:11 +0530

    Adding MACD With Signal line indicator

---
+++ Analyst/src/main/java/Indicators/MACDIndicatorWithSignal.java
@@ -0,0 +1,31 @@
+package Indicators;
+
+import org.ta4j.core.Indicator;
+import org.ta4j.core.indicators.CachedIndicator;
+import org.ta4j.core.indicators.EMAIndicator;
+import org.ta4j.core.indicators.MACDIndicator;
+import org.ta4j.core.num.Num;
+
+public class MACDIndicatorWithSignal extends CachedIndicator<Num> {
+
+	private static final long serialVersionUID = 3319328601246281474L;
+
+	private MACDIndicator macd;
+
+	private EMAIndicator emaMacd;
+
+	public MACDIndicatorWithSignal(Indicator<Num> indicator) {
+		this(indicator, 12, 26, 9);
+	}
+
+	public MACDIndicatorWithSignal(Indicator<Num> indicator, int shortEma, int longEma, int signalLine) {
+		super(indicator);
+		macd = new MACDIndicator(indicator, shortEma, longEma);
+		emaMacd = new EMAIndicator(macd, 9);
+	}
+
+	@Override
+	protected Num calculate(int index) {
+		return macd.getValue(index).minus(emaMacd.getValue(index));
+	}
+}
