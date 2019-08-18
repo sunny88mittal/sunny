ca049fbc4b6529e7e729131c7a5ac4c9a6191595
sunny88mittal <sunny88mittal@gmail.com>
2019-08-18 09:41:52 +0530

    Adding entity for FNO data

---
+++ Analyst/src/main/java/Entities/FNOData.java
@@ -0,0 +1,35 @@
+package Entities;
+
+public class FNOData {
+
+	public String INSTRUMENT;
+
+	public String SYMBOL;
+
+	public String EXPIRY_DT;
+
+	public String STRIKE_PR;
+
+	public String OPTION_TYP;
+
+	public float OPEN;
+
+	public float HIGH;
+
+	public float LOW;
+
+	public float CLOSE;
+
+	public float SETTLE_PR;
+
+	public float CONTRACTS;
+
+	public float VAL_INLAKH;
+
+	public float OPEN_INT;
+
+	public float CHG_IN_OI;
+
+	public String TIMESTAMP;
+
+}
