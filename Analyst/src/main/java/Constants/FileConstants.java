8b904a21ce0758052162e0ba646cd9cd1a46931d
sunny88mittal <sunny88mittal@gmail.com>
2019-08-25 10:45:38 +0530

    Added Past analysis location

--- Analyst/src/main/java/Constants/FileConstants.java
+++ Analyst/src/main/java/Constants/FileConstants.java
@@ -12,6 +12,8 @@ public class FileConstants {
 	
 	public static String DAILY_ANALYSIS_FILE_BASE_PATH = ANALYSIS_FILE_BASE_PATH + "DailyAnalysis\\";
 	
+	public static String PAST_ANALYSIS_FILE_BASE_PATH = ANALYSIS_FILE_BASE_PATH + "PastAnalysis\\";
+	
 	public static String OPTIONS_FILE_BASE_PATH = FILE_BASE_PATH + "Options\\";
 	
 	public static String FNO_FILE_FORMAT = "foDATEMONTHYEARbhav.csv";
