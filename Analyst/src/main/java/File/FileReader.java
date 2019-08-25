18c5c5b1baba2152f2ddf0cfcf2a40d72b401735
sunny88mittal <sunny88mittal@gmail.com>
2019-08-25 14:21:15 +0530

    Added caching

--- Analyst/src/main/java/File/FileReader.java
+++ Analyst/src/main/java/File/FileReader.java
@@ -2,17 +2,25 @@ package File;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.time.DayOfWeek;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import Constants.FileConstants;
 
 public class FileReader {
 
+	private static Map<String, String> candleStickCache = new HashMap<String, String>();
+	private static Map<LocalDate, List<String>> fnoCache = new HashMap<LocalDate, List<String>>();
+
 	public static String getCandeStickData(String fileLocation) {
+		if (candleStickCache.containsKey(fileLocation)) {
+			return candleStickCache.get(fileLocation);
+		}
+
 		String line = "";
 		try {
 			Scanner scanner = new Scanner(new File(fileLocation));
@@ -25,57 +33,56 @@ public class FileReader {
 			e.printStackTrace();
 		}
 
+		candleStickCache.put(fileLocation, line);
 		return line;
 	}
 
-	public static List<String> getFNOData(String fileLocation) {
-		List<String> fnoData = new ArrayList<String>();
-
-		if (fileLocation == null) {
-			LocalDateTime date = LocalDateTime.now();
-
-			int dateValue = date.getDayOfMonth();
-			if (date.getDayOfWeek() == DayOfWeek.SATURDAY) {
-				dateValue -= 1;
-			} else if (date.getDayOfWeek() == DayOfWeek.SUNDAY) {
-				dateValue -= 2;
-			}
-
-			int yearValue = date.getYear();
-
-			String month = date.getMonth().toString();
-			month = month.subSequence(0, 3).toString().toUpperCase();
-			
-			String dateString = dateValue + "";
-			if (dateValue < 10) {
-				dateString = "0" + dateValue;
-			}
-
-			String fileName = FileConstants.FNO_FILE_FORMAT.replaceAll("DATE", dateString)
-					.replaceAll("YEAR", "" + yearValue).replaceAll("MONTH", month);
-			fileLocation = FileConstants.FNO_BASE_PATH + fileName;
+	public static List<String> getFNOData(LocalDate date) throws FileNotFoundException {
+		if (fnoCache.containsKey(date)) {
+			return fnoCache.get(date);
 		}
 
-		try {
-			Scanner scanner = new Scanner(new File(fileLocation));
-			while (scanner.hasNextLine()) {
-				fnoData.add(scanner.nextLine());
-			}
-			scanner.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		int dateValue = date.getDayOfMonth();
+		int yearValue = date.getYear();
+		String month = date.getMonth().toString();
+		month = month.subSequence(0, 3).toString().toUpperCase();
+		String dateString = dateValue + "";
+		if (dateValue < 10) {
+			dateString = "0" + dateValue;
 		}
+
+		String fileName = FileConstants.FNO_FILE_FORMAT.replaceAll("DATE", dateString)
+				.replaceAll("YEAR", "" + yearValue).replaceAll("MONTH", month);
+		String fileLocation = FileConstants.FNO_BASE_PATH + fileName;
+
+		List<String> fnoData = getFNOData(fileLocation);
+		fnoCache.put(date, fnoData);
 
 		return fnoData;
 	}
 
-	public static void main(String args[]) {
-		/*String file = "C:\\Users\\sunmitta\\Desktop\\Perosnal\\Stocks\\Data\\BAJFINANCE\\3minute.json";
-		List<CandleStickData> candleStickDataList = DataUtil.getCandeStickData(file);
-		System.out.println(candleStickDataList.get(10));
-		TimeSeries series = DataUtil.convertToTimeseries(candleStickDataList);
-		System.out.println(series.getBarCount());*/
-		
+	private static List<String> getFNOData(String fileLocation) throws FileNotFoundException {
+		List<String> fnoData = new ArrayList<String>();
+		Scanner scanner = new Scanner(new File(fileLocation));
+		while (scanner.hasNextLine()) {
+			fnoData.add(scanner.nextLine());
+		}
+		scanner.close();
+
+		return fnoData;
+	}
+
+	public static void main(String args[]) throws FileNotFoundException {
+
+		/*
+		 * String file =
+		 * "C:\\Users\\sunmitta\\Desktop\\Perosnal\\Stocks\\Data\\BAJFINANCE\\3minute.json";
+		 * List<CandleStickData> candleStickDataList = DataUtil.getCandeStickData(file);
+		 * System.out.println(candleStickDataList.get(10)); TimeSeries series =
+		 * DataUtil.convertToTimeseries(candleStickDataList);
+		 * System.out.println(series.getBarCount());
+		 */
+
 		getFNOData("C:\\Users\\sunmitta\\Desktop\\Perosnal\\Stocks\\FnoData\\fo16AUG2019bhav.csv");
 	}
 }
