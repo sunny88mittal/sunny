d8551902b244418cb4337a817b9fa39c273a3b89
sunny88mittal <sunny88mittal@gmail.com>
2019-09-03 22:17:28 +0530

    Added Classes for strategy execution

---
+++ Analyst/src/main/java/Strategy/Strategy.java
@@ -0,0 +1,12 @@
+package Strategy;
+
+import org.ta4j.core.Rule;
+
+public class Strategy {
+
+	public Rule entryRule;
+
+	public Rule stopLossRule;
+
+	public Rule exitRule;
+}
---
+++ Analyst/src/main/java/Strategy/StrategyExecutor.java
@@ -0,0 +1,10 @@
+package Strategy;
+
+import Trading.TradesCollector;
+
+public class StrategyExecutor {
+
+	public static TradesCollector executeStrategy(Strategy strategy) {
+		return null;
+	}
+}
