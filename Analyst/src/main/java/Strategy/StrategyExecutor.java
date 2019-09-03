741b28e039fea6e00a11b3813729e88bbff69375
sunny88mittal <sunny88mittal@gmail.com>
2019-09-03 22:19:00 +0530

    Minor change

--- Analyst/src/main/java/Strategy/StrategyExecutor.java
+++ Analyst/src/main/java/Strategy/StrategyExecutor.java
@@ -4,7 +4,7 @@ import Trading.TradesCollector;
 
 public class StrategyExecutor {
 
-	public static TradesCollector executeStrategy(Strategy strategy) {
+	public static TradesCollector executeStrategy(Strategy strategy, String stock, String interval) {
 		return null;
 	}
 }
