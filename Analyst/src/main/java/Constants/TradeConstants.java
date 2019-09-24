1ba4aec3bfa3fa4156442815d95df153b2f11298
sunny88mittal <sunny88mittal@gmail.com>
2019-09-24 19:35:19 +0530

    Adding a file for trade constants

---
+++ Analyst/src/main/java/Constants/TradeConstants.java
@@ -0,0 +1,26 @@
+package Constants;
+
+public class TradeConstants {
+
+	public static final String CALL = "CALL";
+
+	public static final String PUT = "PUT";
+
+	public static final String LONG_BUILDUP = "Long Buildup";
+
+	public static final String LONG_UNWINDING = "Long Unwinding";
+
+	public static final String SHORT_BUILDUP = "Short Buildup";
+
+	public static final String SHORT_UNWINDING = "Short Unwinding";
+
+	public static final String BULLISH = "Bullish";
+
+	public static final String BEARISH = "Bearish";
+
+	public static final String BUY = "BUY";
+
+	public static final String SELL = "SELL";
+
+	public static final String HOLD = "HOLD";
+}
