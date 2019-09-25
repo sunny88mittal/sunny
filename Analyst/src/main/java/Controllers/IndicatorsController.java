c68523c54866d5e315b9d54a76aaf317e9af76bd
sunny88mittal <sunny88mittal@gmail.com>
2019-09-25 10:45:00 +0530

    Minor changes

--- Analyst/src/main/java/Controllers/IndicatorsController.java
+++ Analyst/src/main/java/Controllers/IndicatorsController.java
@@ -3,6 +3,7 @@ package Controllers;
 import java.io.IOException;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import Data.RealTimeIndicatorProvider;
 @RequestMapping("/indicators")
 public class IndicatorsController {
 
-	@RequestMapping("/")
+	@GetMapping("/get/data")
 	public Map<String, Map<String, String>> getIndicators() throws IOException {
 		return RealTimeIndicatorProvider.getIndicatorsFor();
 	}
--- Analyst/src/main/java/Controllers/OptionsChainController.java
+++ Analyst/src/main/java/Controllers/OptionsChainController.java
@@ -3,6 +3,7 @@ package Controllers;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +15,12 @@ import Entities.OptionsChainInterpretation;
 @RequestMapping("/optionschain")
 public class OptionsChainController {
 
-	@RequestMapping("/data")
+	@GetMapping("/get/data")
 	OptionsChain getOptionsChainData() throws IOException, InterruptedException {
 		return OptionsChainDownloader.getOptionsChain();
 	}
 
-	@RequestMapping("/interpretations")
+	@GetMapping("/get/interpretations")
 	List<OptionsChainInterpretation> getOptionsChainInterpretations() throws IOException, InterruptedException {
 		return OptionsChainDownloader.getOptionschainInterpretations();
 	}
