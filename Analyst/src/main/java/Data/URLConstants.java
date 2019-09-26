a8cfef80cb1ea7b383ab299b2930f9d3a66d8a75
sunny88mittal <sunny88mittal@gmail.com>
2019-09-26 19:40:33 +0530

    Minor Change

--- Analyst/src/main/java/Data/URLConstants.java
+++ Analyst/src/main/java/Data/URLConstants.java
@@ -11,4 +11,8 @@ public class URLConstants {
 	public static final String REAL_TIME_URL = "https://kitecharts-aws.zerodha.com" + "/api/chart/SYMBOL/INTERVAL?"
 			+ "public_token=px7RlNQX3W7rg9Vm2WOUDbGzo6WBxUqy&" + "user_id=YF3210&oi=1" + "&api_key=kitefront&"
 			+ "access_token=&" + "from=FROMDATE" + "&to=TODATE&" + "ciqrandom=1564301727399";
+
+	public static final String OPTIONS_CHAIN_URL = "https://www.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?"
+			+ "symbolCode=-10006&symbol=SYMBOL&symbol=SYMBOL&instrument=OPTIDX&date=DATE&segmentLink=17&symbolCount=2&segmentLink=17";
+
 }
