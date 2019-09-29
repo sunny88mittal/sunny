7596f1ea8758d1d94023b4901e4b5196503da0e3
sunny88mittal <sunny88mittal@gmail.com>
2019-09-29 14:34:54 +0530

    Minor Change

--- Analyst/src/main/java/Controllers/IndicatorsController.java
+++ Analyst/src/main/java/Controllers/IndicatorsController.java
@@ -1,20 +1,21 @@
 package Controllers;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import Data.RealTimeIndicatorProvider;
+import Entities.RealTimeIndicatorValues;
 
 @RestController
 @RequestMapping("/indicators")
 public class IndicatorsController {
 
 	@GetMapping("/get/data")
-	public Map<String, Map<String, String>> getIndicators() throws IOException {
+	public List<RealTimeIndicatorValues> getIndicators() throws IOException {
 		return RealTimeIndicatorProvider.getIndicatorsFor();
 	}
 }
