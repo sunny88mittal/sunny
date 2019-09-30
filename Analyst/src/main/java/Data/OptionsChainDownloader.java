2d4261bbc3f7918a32901d2624544e8350cbf27b
sunny88mittal <sunny88mittal@gmail.com>
2019-09-30 22:36:07 +0530

    Minor Change

--- Analyst/src/main/java/Data/OptionsChainDownloader.java
+++ Analyst/src/main/java/Data/OptionsChainDownloader.java
@@ -56,6 +56,7 @@ public class OptionsChainDownloader {
 	}
 
 	public static OptionsChain getLatestOptionsChain() throws IOException, InterruptedException {
+		loadDataFromDisk();
 		return lastOptionChain;
 	}
 
