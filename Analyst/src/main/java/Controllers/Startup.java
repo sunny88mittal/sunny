99748d49d3baff5ade6d7dcd214f99514c68ab56
sunny88mittal <sunny88mittal@gmail.com>
2019-10-02 07:07:43 +0530

    Added Thread pool with size 2 for scheduled tasks

--- Analyst/src/main/java/Controllers/Startup.java
+++ Analyst/src/main/java/Controllers/Startup.java
@@ -5,14 +5,25 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableScheduling
 @ComponentScan({ "Data", "Controllers" })
-public class Startup {
+public class Startup implements SchedulingConfigurer {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Startup.class, args);
+	}
+
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+		taskScheduler.setPoolSize(3);
+		taskScheduler.initialize();
+		taskRegistrar.setScheduler(taskScheduler);
 	}
 }
