2b107d87f85b5c99818036a38173e8f3bc259003
root <root@MININT-DS03HK0.fareast.corp.microsoft.com>
2020-04-11 18:28:21 +0530

    Minor change

--- DataStructures/src/com/sunny/array/ArrayRotation.java
+++ DataStructures/src/com/sunny/array/ArrayRotation.java
@@ -3,7 +3,7 @@ package com.sunny.array;
 public class ArrayRotation {
 
 	public static void main(String args[]) {
-		int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
 		rotateArray(arr, 3);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
