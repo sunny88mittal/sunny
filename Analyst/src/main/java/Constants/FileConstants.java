abd3c1c728481c430d28feda7b98f2cfe9cc8086
sunny88mittal <sunny88mittal@gmail.com>
2020-05-24 17:25:11 +0530

    Minor Change

--- Analyst/src/main/java/Constants/FileConstants.java
+++ Analyst/src/main/java/Constants/FileConstants.java
@@ -3,20 +3,22 @@ package Constants;
 public class FileConstants {
 
 	private static String FILE_BASE_PATH = "C:\\Users\\sunmitta\\Desktop\\Perosnal\\Stocks\\";
-	
+
 	private static String DATA_FILE_BASE_PATH = FILE_BASE_PATH + "Data\\";
-	
+
 	public static String ANALYSIS_FILE_BASE_PATH = FILE_BASE_PATH + "Analysis\\";
-	
+
 	public static String FNO_BASE_PATH = DATA_FILE_BASE_PATH + "\\FnoData\\";
 
-	public static String STOCKS_DATA_FILE_BASE_PATH = DATA_FILE_BASE_PATH + "Stocks Data\\";
-	
+	public static String OI_BASE_PATH = DATA_FILE_BASE_PATH + "\\OIData\\";
+
+	public static String STOCKS_DATA_FILE_BASE_PATH = DATA_FILE_BASE_PATH + "StocksRawData\\";
+
 	public static String DAILY_ANALYSIS_FILE_BASE_PATH = ANALYSIS_FILE_BASE_PATH + "DailyAnalysis\\";
-	
+
 	public static String PAST_ANALYSIS_FILE_BASE_PATH = ANALYSIS_FILE_BASE_PATH + "PastAnalysis\\";
-	
+
 	public static String OPTIONS_FILE_BASE_PATH = DATA_FILE_BASE_PATH + "Live Options Chain\\";
-	
+
 	public static String FNO_FILE_FORMAT = "foDATEMONTHYEARbhav.csv";
 }
