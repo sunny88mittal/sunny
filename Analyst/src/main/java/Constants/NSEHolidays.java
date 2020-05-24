94e8f8674a72391fba3695d0bf1081a4234ef491
sunny88mittal <sunny88mittal@gmail.com>
2020-05-24 17:43:52 +0530

    Updated Holiday List

--- Analyst/src/main/java/Constants/NSEHolidays.java
+++ Analyst/src/main/java/Constants/NSEHolidays.java
@@ -10,12 +10,18 @@ public class NSEHolidays {
 	private static Set<String> holidays = new HashSet<String>();
 
 	static {
-		holidays.add("02-10-2019");
-		holidays.add("08-10-2019");
-		holidays.add("21-10-2019");
-		holidays.add("28-10-2019");
-		holidays.add("12-11-2019");
-		holidays.add("25-12-2019");
+		holidays.add("21-02-2020");
+		holidays.add("10-03-2020");
+		holidays.add("02-04-2020");
+		holidays.add("06-04-2020");
+		holidays.add("10-04-2020");
+		holidays.add("14-04-2020");
+		holidays.add("01-05-2020");
+		holidays.add("25-05-2020");
+		holidays.add("02-10-2020");
+		holidays.add("16-11-2020");
+		holidays.add("30-11-2020");
+		holidays.add("25-12-2020");
 	}
 
 	public static boolean isHoliday(LocalDateTime date) {
