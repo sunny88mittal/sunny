ba4a2d14e13e907d3f7b0239624bfddf828c1801
sunny88mittal <sunny88mittal@gmail.com>
2020-07-27 20:14:38 +0530

    New Merge Sort

--- Algorithms/src/Sorting/MergeSort.java
+++ Algorithms/src/Sorting/MergeSort.java
@@ -12,60 +12,58 @@ import java.util.Arrays;
  */
 public class MergeSort {
 
-    public static void main(String args[]) {
-        int[] arr = new int[]{10, 20, 5, 4, 25, 3, 35, 1, 55};
-        int[] result = mergesort(arr);
-        int length = result.length;
-        for (int i = 0; i < length; i++) {
-            System.out.println(result[i]);
-        }
-    }
+	public static void main(String args[]) {
+		int[] arr = new int[] { 10, 20, 5, 4, 25, 3, 35, 1, 55 };
+		int[] result = mergesort(arr);
+		int length = result.length;
+		for (int i = 0; i < length; i++) {
+			System.out.println(result[i]);
+		}
+	}
 
-    private static int[] mergesort(int[] arr) {
-        int start = 0;
-        int end = arr.length;
-        int mid = end / 2;
+	private static int[] mergesort(int[] arr) {
+		int length = arr.length;
+		if (length == 1) {
+			return arr;
+		}
 
-        if (mid == 0) {
-            return arr;
-        } else {
-            return combine(mergesort(Arrays.copyOfRange(arr, start, mid)),
-                    mergesort(Arrays.copyOfRange(arr, mid, end)));
-        }
-    }
+		int mid = length / 2;
+		return combine(mergesort(Arrays.copyOfRange(arr, 0, mid)), mergesort(Arrays.copyOfRange(arr, mid, length)));
+	}
 
-    private static int[] combine(int[] arr1, int[] arr2) {
-        int arr1Length = arr1.length;
-        int arr2Length = arr2.length;
-        int arr1Index = 0;
-        int arr2Index = 0;
-        int index = 0;
-        int[] arr = new int[arr1Length + arr2Length];
+	private static int[] combine(int arr1[], int arr2[]) {
+		int[] arr = new int[arr1.length + arr2.length];
 
-        while (arr1Index < arr1Length && arr2Index < arr2Length) {
-            if (arr1[arr1Index] < arr2[arr2Index]) {
-                arr[index] = arr1[arr1Index];
-                arr1Index++;
-            } else {
-                arr[index] = arr2[arr2Index];
-                arr2Index++;
-            }
-            index++;
-        }
+		int arr1Index = 0;
+		int arr2Index = 0;
+		int combinedArrIndex = 0;
 
-        if (arr1Index < arr1Length) {
-            for (int i = arr1Index; i < arr1Length; i++) {
-                arr[index] = arr1[i];
-                index++;
-            }
-        }
+		// Combine elements from arr1 and arr2
+		while (arr1Index < arr1.length && arr2Index < arr2.length) {
+			if (arr1[arr1Index] < arr2[arr2Index]) {
+				arr[combinedArrIndex] = arr1[arr1Index];
+				arr1Index++;
+			} else {
+				arr[combinedArrIndex] = arr2[arr2Index];
+				arr2Index++;
+			}
+			combinedArrIndex++;
+		}
 
-        if (arr2Index < arr2Length) {
-            for (int i = arr2Index; i < arr2Length; i++) {
-                arr[index] = arr2[i];
-                index++;
-            }
-        }
-        return arr;
-    }
+		// Remaining elements from array1
+		while (arr1Index < arr1.length) {
+			arr[combinedArrIndex] = arr1[arr1Index];
+			arr1Index++;
+			combinedArrIndex++;
+		}
+
+		// Remaining elements from array2
+		while (arr2Index < arr2.length) {
+			arr[combinedArrIndex] = arr2[arr2Index];
+			arr2Index++;
+			combinedArrIndex++;
+		}
+
+		return arr;
+	}
 }
