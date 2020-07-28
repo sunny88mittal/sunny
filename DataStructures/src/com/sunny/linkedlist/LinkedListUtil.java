1da835f6dca88486fe7e97b7b3438a43ec63d4a5
sunny88mittal <sunny88mittal@gmail.com>
2020-07-28 22:00:18 +0530

    Minor Change

--- DataStructures/src/com/sunny/linkedlist/LinkedListUtil.java
+++ DataStructures/src/com/sunny/linkedlist/LinkedListUtil.java
@@ -14,4 +14,17 @@ public class LinkedListUtil {
 
 		return head;
 	}
+
+	public static void printLinkedList(Node head) {
+		while (head != null) {
+			System.out.println(head.value);
+			head = head.next;
+		}
+	}
+
+	public static void main(String args[]) {
+		int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+		Node linkedList = createLinkedList(numbers);
+		printLinkedList(linkedList);
+	}
 }
