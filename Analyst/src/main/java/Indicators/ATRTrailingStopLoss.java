694ce66dbc3c354a1c26b4727de45a59cc951089
sunny88mittal <sunny88mittal@gmail.com>
2020-09-06 20:42:27 +0530

    ATR Trailing Stop Loss Indicator

--- Analyst/src/main/java/Indicators/ATRTrailingStopLoss.java
+++ Analyst/src/main/java/Indicators/ATRTrailingStopLoss.java
@@ -18,7 +18,7 @@ public class ATRTrailingStopLoss extends CachedIndicator<Num> {
 
 	private static final long serialVersionUID = -8939946592454023602L;
 
-	private int multiplier;
+	private Num multiplier;
 
 	private TimeSeries series;
 
@@ -26,58 +26,49 @@ public class ATRTrailingStopLoss extends CachedIndicator<Num> {
 
 	public ATRTrailingStopLoss(TimeSeries series, int period, int multiplier) {
 		super(series);
-		this.multiplier = multiplier;
+		this.multiplier = PrecisionNum.valueOf(multiplier);
 		this.series = series;
 		atrIndicator = new ATRIndicator(series, period);
 	}
 
 	@Override
 	protected Num calculate(int index) {
-		Num[] atrTSLs = new PrecisionNum[series.getBarCount()];
-		atrTSLs[0] = PrecisionNum.valueOf(0);
+		Num invertedAt = series.getBar(0).getClosePrice();
+		Num atrTSL = invertedAt.minus(atrIndicator.getValue(0).multipliedBy(multiplier));
 
-		for (int i = 0; i < 7; i++) {
-			Num close = series.getBar(i).getClosePrice();
-			Num atr = atrIndicator.getValue(i).multipliedBy(PrecisionNum.valueOf(multiplier));
-			atrTSLs[i] = close.minus(atr);
-		}
-
-		// Get atrTSLs
-		for (int i = 7; i < series.getBarCount(); i++) {
+		for (int i = 1; i <= index; i++) {
+			Num curClose = series.getBar(i).getClosePrice();
 			Num prevClose = series.getBar(i - 1).getClosePrice();
-			Num prevATRTSL = atrTSLs[i - 1];
-
-			Num close = series.getBar(i).getClosePrice();
-			Num atr = atrIndicator.getValue(i).multipliedBy(PrecisionNum.valueOf(multiplier));
-			atrTSLs[i] = prevATRTSL;
-
-			if (close.isLessThan(prevClose)) {
-				if (prevATRTSL.isLessThan(close)) { // Down candle in up trend, ATRTSL remains same
-					atrTSLs[i] = prevATRTSL;
-				} else if (prevATRTSL.isGreaterThan(close)) { // Down candle in down trend, ATRTSL falls, trend changes
-					atrTSLs[i] = close.plus(atr);
+			Num atrWithMultiplier = atrIndicator.getValue(i).multipliedBy(multiplier);
+			if (curClose.isLessThan(prevClose)) {
+				if (atrTSL.isGreaterThan(curClose)) { // Down trend starts or continues
+					if (curClose.isLessThan(invertedAt)) {
+						invertedAt = curClose;
+						atrTSL = curClose.plus(atrWithMultiplier);
+					}
 				}
-			} else if (close.isGreaterThan(prevClose)) {
-				if (prevATRTSL.isLessThan(close)) { // Up candle in up trend, ATRTSL remains same
-					atrTSLs[i] = prevATRTSL;
-				} else if (prevATRTSL.isLessThan(close)) { // Up candle in down trend, ATRTSL falls
-					atrTSLs[i] = close.minus(atr);
+			} else {
+				if (atrTSL.isLessThan(curClose)) {
+					if (curClose.isGreaterThan(invertedAt)) { // up trend starts or continues
+						invertedAt = curClose;
+						atrTSL = curClose.minus(atrWithMultiplier);
+					}
 				}
 			}
 		}
 
-		//return atrTSLs[index];
-		return atrIndicator.getValue(index);
+		return atrTSL;
 	}
 
 	public static void main(String args[]) {
 		TimeSeries timeSeries = DataUtil.getTimeSeries(StockSymbols.RELIANCE.name, CandleStickInterval.DAY);
-		ATRTrailingStopLoss indicator = new ATRTrailingStopLoss(timeSeries, 7, 3);
 		int length = timeSeries.getBarCount();
+		ATRTrailingStopLoss atrTrailingStopLoss = new ATRTrailingStopLoss(timeSeries, 7, 3);
 		for (int i = 0; i < length; i++) {
 			Bar bar = timeSeries.getBar(i);
 			System.out.println(
-					bar.getSimpleDateName() + "	" + bar.getClosePrice().intValue() + "	" + indicator.getValue(i).intValue());
+					bar.getSimpleDateName() + "," + bar.getClosePrice().intValue() + "," + bar.getMaxPrice().intValue()
+							+ "," + bar.getMinPrice().intValue() + "," + atrTrailingStopLoss.getValue(i).intValue());
 		}
 	}
 }
