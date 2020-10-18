cccca1708524c5507a9f412427bf4fbeffb6b0d0
sunny88mittal <sunny88mittal@gmail.com>
2020-10-18 11:25:39 +0530

    Added method to get instrument specific data

--- Analyst/src/main/java/DataProvider/FNODataProvider.java
+++ Analyst/src/main/java/DataProvider/FNODataProvider.java
@@ -3,12 +3,21 @@ package DataProvider;
 import java.io.FileNotFoundException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import DataUtil.DataUtil;
 import Entities.FNOData;
 import File.FileReader;
 
 public class FNODataProvider {
+
+	public static List<FNOData> getFNOData(String instrument, String symbol, LocalDate date)
+			throws FileNotFoundException {
+		List<FNOData> fnoData = getFNOData(date);
+		List<FNOData> filteredData = fnoData.stream()
+				.filter(e -> e.INSTRUMENT.equals(instrument) && e.SYMBOL.equals(symbol)).collect(Collectors.toList());
+		return filteredData;
+	}
 
 	public static List<FNOData> getFNOData(LocalDate date) throws FileNotFoundException {
 		List<String> rawData = FileReader.getFNOData(date);
