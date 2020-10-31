a0f1b4f5aa1b805b09d913310814f930847b3c49
sunny88mittal <sunny88mittal@gmail.com>
2020-10-31 10:03:46 +0530

    Minor Change

--- Analyst/src/main/java/StrategyAnalyzer/NormalDistributionStrategyAnalyzer.java
+++ Analyst/src/main/java/StrategyAnalyzer/NormalDistributionStrategyAnalyzer.java
@@ -2,6 +2,7 @@ package StrategyAnalyzer;
 
 import java.io.FileNotFoundException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.ta4j.core.Bar;
@@ -17,18 +18,12 @@ public class NormalDistributionStrategyAnalyzer {
 
 	private static void analyze(String instrument, int noOfWeeks, float percentage) throws FileNotFoundException {
 		TimeSeries series = DataUtil.getTimeSeries(instrument, CandleStickInterval.DAY);
-		List<FNOData> fnoData = FNODataProvider.getFNOData("OPTIDX", instrument, LocalDate.now().minusDays(2));
-		for (int i = 0; i < noOfWeeks; i++) {
-			getLastWeeksStratingBar(series, i);
-		}
+		List<Bar> expiryBars = getExpiryDayBars(series);
 	}
 
-	private static Bar getLastWeeksStratingBar(TimeSeries series, int weekOffset) {
-		return null;
-	}
-
-	private Bar getLastWeeksEndingBar(TimeSeries series, int weekOffset) {
-		return null;
+	private static List<Bar> getExpiryDayBars(TimeSeries series) {
+		List<Bar> expiryBars = new ArrayList<Bar>();
+		return expiryBars;
 	}
 
 	public static void main(String args[]) throws FileNotFoundException {
