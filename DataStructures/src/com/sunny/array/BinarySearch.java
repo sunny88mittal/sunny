8c900a5917796596fdfcc73e9785720fea3f3043
sunny88mittal <sunny88mittal@gmail.com>
2021-09-25 15:24:59 +0530

    Minor change

--- DataStructures/src/com/sunny/array/BinarySearch.java
+++ DataStructures/src/com/sunny/array/BinarySearch.java
@@ -1,26 +1,27 @@
 package com.sunny.array;
 
+import junit.framework.Assert;
+
 public class BinarySearch {
 
 	public static void main(String args[]) {
 		int arr[] = new int[] { 1, 2, 3, 5, 8, 12, 14, 16, 20, 22, 27, 32 };
-		System.out.println(binarySearch(0, arr.length - 1, arr, 16));
-		System.out.println(binarySearch(0, arr.length - 1, arr, 15));
-		System.out.println(binarySearch(0, arr.length - 1, arr, 1));
-		System.out.println(binarySearch(0, arr.length - 1, arr, 27));
-		System.out.println(binarySearch(0, arr.length - 1, arr, 32));
-		System.out.println(binarySearch(0, arr.length - 1, arr, 35));
+	
+		Assert.assertEquals(binarySearch(0, arr.length - 1, arr, 16), 7);
+		Assert.assertEquals(binarySearch(0, arr.length - 1, arr, 15), -1);
+		Assert.assertEquals(binarySearch(0, arr.length - 1, arr, 1), 0);
+		Assert.assertEquals(binarySearch(0, arr.length - 1, arr, 27), 10);
+		Assert.assertEquals(binarySearch(0, arr.length - 1, arr, 32), 11);
+		Assert.assertEquals(binarySearch(0, arr.length - 1, arr, 35), -1);
 	}
 
 	private static int binarySearch(int start, int end, int[] arr, int element) {
 		int mid = (start + end) / 2;
 
-		// Match found return the index
 		if (element == arr[mid]) {
 			return mid;
 		}
 
-		// Array exhausted
 		if (start == end) {
 			return -1;
 		}
