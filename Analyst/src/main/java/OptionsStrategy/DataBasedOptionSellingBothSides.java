30797dfae0fe37275b9c8c3c3d2b6a3681f6ba2e
sunny88mittal <sunny88mittal@gmail.com>
2022-04-19 18:32:12 +0530

    Added one more algo

--- Analyst/src/main/java/OptionsStrategy/DataBasedOptionSellingBothSides.java
+++ Analyst/src/main/java/OptionsStrategy/DataBasedOptionSellingBothSides.java
@@ -1,13 +1,5 @@
 package OptionsStrategy;
 
-/**
- * Sell an option at 9:30 AM based upon which side more options are written
- * Close the trade
- *   1. If stop loss hits
- *   2. If data reverses
- *   3. If day ends i.e 3:29 PM 
- */
-
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,15 +7,19 @@ import java.util.List;
 import Constants.StockSymbols;
 import Entities.OptionsChain;
 
-public class DataBasedOptionSelling {
+public class DataBasedOptionSellingBothSides {
 
 	private List<Trade> trades = new ArrayList<Trade>();
 
 	public List<Trade> execute(String date) {
 		boolean isTradeOpen = false;
+		boolean isReversalTradeOpen = false;
 
 		for (int i = 0; i < 10000; i++) {
 			OptionsChain optionsChain = OptionsDataProvider.getData(StockSymbols.BANKNIFTY.name, date, i);
+			if (optionsChain == null) {
+				break;
+			}
 			Timestamp ts = new Timestamp(optionsChain.timeStamp);
 			int hours = ts.getHours();
 			int minutes = ts.getMinutes();
@@ -31,36 +27,21 @@ public class DataBasedOptionSelling {
 			double callOIChange = OptionsChainHelper.getOpenInterestChange(optionsChain.callOptions);
 			double putOIChange = OptionsChainHelper.getOpenInterestChange(optionsChain.putOptions);
 			int price = (int) Double.parseDouble(optionsChain.price);
-			
-			// Open a trade
+
+			// Open a trade at 9:30 AM
 			if (!isTradeOpen && hours >= 9 && minutes >= 30) {
-				int strike = (price / 100) * 100;
-				double sellingPrice = 0;
-				Trade trade = new Trade();
-				trade.entry = price;
-
-				if (callOIChange > putOIChange) {
-					strike = (price / 100) * 100 + 100;
-					sellingPrice = OptionsChainHelper.getCEPrice(optionsChain, strike);
-					trade.ceEntryPrice = sellingPrice;
-					trade.ceStopLoss = sellingPrice + 100;
-					trade.strike = strike;
-					System.out.println("Selling Call : " + strike + " at : " + sellingPrice);
-				} else {
-					sellingPrice = OptionsChainHelper.getPEPrice(optionsChain, strike);
-					trade.peEntryPrice = sellingPrice;
-					trade.peStopLoss = sellingPrice + 100;
-					trade.strike = strike;
-					System.out.println("Selling Put : " + strike + " at : " + sellingPrice);
-				}
-
+				Trade trade = initiateTrade(price, optionsChain, callOIChange, putOIChange);
 				trades.add(trade);
 				isTradeOpen = true;
 			}
 
-			// Close the trade
-			if (isTradeOpen) {
-				Trade trade = trades.get(0);
+			Trade newTrade = null;
+			// Close the trade/s
+			for (Trade trade : trades) {
+
+				if (!isTradeOpen(trade)) {
+					continue;
+				}
 
 				// If time over close
 				if (hours >= 15 && minutes >= 29) {
@@ -73,8 +54,6 @@ public class DataBasedOptionSelling {
 					if (trade.peEntryPrice > 1) {
 						trade.peExitPrice = OptionsChainHelper.getPEPrice(optionsChain, trade.strike);
 					}
-
-					break;
 				}
 
 				// If stop loss hits
@@ -83,7 +62,6 @@ public class DataBasedOptionSelling {
 					System.out.println("Stop Loss hits");
 					trade.ceExitPrice = OptionsChainHelper.getCEPrice(optionsChain, trade.strike);
 					trade.exit = price;
-					break;
 				}
 
 				if (trade.peEntryPrice > 1
@@ -91,31 +69,68 @@ public class DataBasedOptionSelling {
 					trade.peExitPrice = OptionsChainHelper.getPEPrice(optionsChain, trade.strike);
 					System.out.println("Stop Loss hits");
 					trade.exit = price;
-					break;
 				}
 
 				// If data reversal happens
-				if (trade.ceEntryPrice > 1 && putOIChange > callOIChange) {
+				if (trade.ceEntryPrice > 1 && putOIChange > callOIChange && !isReversalTradeOpen) {
 					System.out.println("Data reversal");
-					trade.ceExitPrice = OptionsChainHelper.getCEPrice(optionsChain, trade.strike);
-					trade.exit = price;
-					break;
+					newTrade = initiateTrade(price, optionsChain, callOIChange, putOIChange);
+					isReversalTradeOpen = true;
 				}
-				
-				if (trade.peEntryPrice > 1 && putOIChange < callOIChange) {
+
+				if (trade.peEntryPrice > 1 && putOIChange < callOIChange && !isReversalTradeOpen) {
 					System.out.println("Data reversal");
-					trade.exit = price;
-					trade.peExitPrice = OptionsChainHelper.getCEPrice(optionsChain, trade.strike);
-					break;
+					newTrade = initiateTrade(price, optionsChain, callOIChange, putOIChange);
+					isReversalTradeOpen = true;
 				}
+			}
+
+			if (newTrade != null) {
+				trades.add(newTrade);
 			}
 		}
 
 		return trades;
 	}
-	
+
+	private static boolean isTradeOpen(Trade trade) {
+		if (trade.ceEntryPrice > 1 && trade.ceExitPrice > 1) {
+			return false;
+		}
+
+		if (trade.peEntryPrice > 1 && trade.peExitPrice > 1) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private static Trade initiateTrade(int price, OptionsChain optionsChain, double callOIChange, double putOIChange) {
+		int strike = (price / 100) * 100;
+		double sellingPrice = 0;
+		Trade trade = new Trade();
+		trade.entry = price;
+
+		if (callOIChange > putOIChange) {
+			strike = (price / 100) * 100 + 100;
+			sellingPrice = OptionsChainHelper.getCEPrice(optionsChain, strike);
+			trade.ceEntryPrice = sellingPrice;
+			trade.ceStopLoss = sellingPrice + 100;
+			trade.strike = strike;
+			System.out.println("Selling Call : " + strike + " at : " + sellingPrice);
+		} else {
+			sellingPrice = OptionsChainHelper.getPEPrice(optionsChain, strike);
+			trade.peEntryPrice = sellingPrice;
+			trade.peStopLoss = sellingPrice + 100;
+			trade.strike = strike;
+			System.out.println("Selling Put : " + strike + " at : " + sellingPrice);
+		}
+
+		return trade;
+	}
+
 	public static void main(String args[]) {
-		DataBasedOptionSelling dbos = new DataBasedOptionSelling();
+		DataBasedOptionSellingBothSides dbos = new DataBasedOptionSellingBothSides();
 		List<Trade> trades = dbos.execute("19-04-2022");
 		int netProfit = 0;
 		for (Trade trade : trades) {
