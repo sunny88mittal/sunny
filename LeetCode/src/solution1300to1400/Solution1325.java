f43f56028fa6217b344965760136460c65389bfb
sunny88mittal <sunny88mittal@gmail.com>
2023-12-06 22:48:35 +0530

    Adding one more solution

---
+++ LeetCode/src/solution1300to1400/Solution1325.java
@@ -0,0 +1,38 @@
+package solution1300to1400;
+
+public class Solution1325 {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public TreeNode removeLeafNodes(TreeNode root, int target) {
+		if (root.left != null) {
+			root.left = removeLeafNodes(root.left, target);
+		}
+
+		if (root.right != null) {
+			root.right = removeLeafNodes(root.right, target);
+		}
+
+		if (root.left == null && root.right == null && root.val == target) {
+			return null;
+		}
+
+		return root;
+	}
+}
